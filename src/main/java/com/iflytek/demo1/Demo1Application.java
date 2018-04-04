2bb02d66cd0abc6a5f54f9dc63a520d457b9d428
jhjiang <a599177728@sina.com>
2018-04-04 17:40:34 +0800

    dsa
    fdsaf

--- src/main/java/com/iflytek/demo1/Demo1Application.java
+++ src/main/java/com/iflytek/demo1/Demo1Application.java
@@ -3,10 +3,8 @@ package com.iflytek.demo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.boot.context.embedded.*;
 
 @Controller
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
@@ -19,10 +17,5 @@ public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
-	}
-
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer(){
-
 	}
 }
